6af4b1de578498a81f427e0c955958f7ae03459c
ruddro42001~ <aliruddro@gmail.com>
2025-03-17 16:25:38 +0600

    new look

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -27,7 +27,16 @@ const Products = () => {
     return (
         <div style={{ marginTop: '30px' }}>
             <div class="container mx-auto px-8">
-                <h2 className='text-4xl font-semibold text-gray-800 py-8'>All Products</h2>
+                <div class="mb-4 flex items-center justify-between gap-4 md:mb-8">
+                    <h2 class="text-xl font-semibold text-gray-900 dark:text-white sm:text-2xl">All Products</h2>
+
+                    <a href="#" title="" class="flex items-center text-base font-medium text-primary-700 hover:underline dark:text-primary-500">
+                        See more products
+                        <svg class="ms-1 h-5 w-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 12H5m14 0-4 4m4-4-4-4" />
+                        </svg>
+                    </a>
+                </div>
                 <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
                     {loading ? (
                         <div className="loader-container">
@@ -37,20 +46,24 @@ const Products = () => {
                     ) : (
                         <>
                             {data.map((item) => (
-                                <Link to={`/single/${item.id}`} key={item.id} class="bg-white p-6 rounded-lg shadow-lg hover:shadow-2xl transition duration-300 ease-in-out transform hover:scale-105">
-                                    <img class="w-full h-56 rounded-lg" src={item.image} alt="Product Image" />
-                                    <h3 class="text-xl font-semibold text-gray-800 mt-4">{item.title.substring(0, 18)} ...</h3>
+                                <div key={item.id} class="bg-white p-6 rounded-lg shadow-lg hover:shadow-2xl transition duration-300 ease-in-out transform hover:scale-105">
+                                    <Link to={`/single/${item.id}`}>
+                                        <img class="w-full h-56 rounded-lg" src={item.image} alt="Product Image" />
+                                    </Link>
+                                    <Link to={`/single/${item.id}`}>
+                                        <h3 class="text-xl font-semibold text-gray-800 mt-4">{item.title.substring(0, 18)} ...</h3>
+                                    </Link>
                                     <p class="text-sm text-gray-600 mt-1">{item.category}</p>
                                     <p class="text-xl font-semibold text-gray-900 mt-2">$ {item.price}</p>
                                     <div class="mt-4 flex justify-between items-center">
                                         <button
                                             onClick={() => addToCart(item)}
-                                            class="bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 transition duration-300">Add to Cart</button>
+                                            class="bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 transition duration-300 cursor-pointer">Add to Cart</button>
                                         <button
                                             onClick={() => orderNow(item)}
-                                            class="bg-green-500 text-white py-2 px-4 rounded-md hover:bg-green-600 transition duration-300">Order Now</button>
+                                            class="bg-green-500 text-white py-2 px-4 rounded-md hover:bg-green-600 transition duration-300 cursor-pointer">Order Now</button>
                                     </div>
-                                </Link>
+                                </div>
                             ))}
                         </>
                     )}
